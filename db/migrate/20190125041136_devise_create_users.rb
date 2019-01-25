cafb79fc7c76f4a29f084f04dd7bb777faae2959
59050336 <59050336@kmitl.ac.th>
2019-01-25 11:34:07 +0700

    update89

--- db/migrate/20190125041136_devise_create_users.rb
+++ db/migrate/20190125041136_devise_create_users.rb
@@ -9,7 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       ## Custom fields
       t.string :name
-      
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
