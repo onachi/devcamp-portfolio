c7c3ce277479e213b912b8ea5f4528f00cc600bf
59050336 <59050336@kmitl.ac.th>
2019-01-30 17:18:28 +0700

    up122

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,20 @@ class User < ApplicationRecord
   validates_presence_of :name
 
   def first_name
-    self.name.split.first
+    self.name.split.first.oops
   end
 
   def last_name
     self.name.split.last
+  end
+
+  def get_facebook_messages
+    begin
+      contacts_fb
+      @messages = retrieves_messages
+    rescue => e
+      flash[:error] = "Error occurred contacting Facebook: #{e}"
+    end
   end
 
 end
