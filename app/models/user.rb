1268ed59bdb362294f7f2246bec48fdeb1ae7382
59050336 <59050336@kmitl.ac.th>
2019-01-30 17:21:32 +0700

    up122

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name
 
   def first_name
-    self.name.split.first.oops
+    self.name.split.first
   end
 
   def last_name
     self.name.split.last
   end
-
-  def get_facebook_messages
-    begin
-      contacts_fb
-      @messages = retrieves_messages
-    rescue => e
-      flash[:error] = "Error occurred contacting Facebook: #{e}"
-    end
-  end
-
 end
