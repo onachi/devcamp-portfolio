175a66e3e68217fe433cfffc893836fe78b7fb6d
59050336 <59050336@kmitl.ac.th>
2019-01-31 10:13:16 +0700

    authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyrignt = DevcampViewTool::Renderer.copyright 'oat aanuchit','All rights reserved'
+  end
+end
+
+module DevcampViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&coppy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
