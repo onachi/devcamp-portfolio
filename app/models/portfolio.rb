d7278e752880187c5e6aadc7eadcfbfdc374f8ce
59050336 <59050336@kmitl.ac.th>
2019-02-13 14:26:51 +0700

    update171

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,13 +1,20 @@
 class Portfolio < ApplicationRecord
     has_many :technologies
     accepts_nested_attributes_for :technologies,
-                                              reject_if: lambda { |attrs| attrs['name'].blank? }
+                                  reject_if: lambda { |attrs| attrs['name'].blank? }
 
     include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
 
+    mount_uploader :thumb_image, PortfolioUploader
+    mount_uploader :main_image, PortfolioUploader
+
     def self.angular
         where(subtitle: 'Angular')
+    end
+
+    def self.by_position
+        order("position ASC")
     end
 
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
