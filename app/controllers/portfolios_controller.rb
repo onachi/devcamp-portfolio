31cb01f95b5aac02c72cbae87920a4e7b6ed6d55
59050336 <59050336@kmitl.ac.th>
2019-02-14 11:51:04 +0700

    update180

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,6 +13,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -59,6 +60,8 @@ class PortfoliosController < ApplicationController
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:id, :name, :_destroy]
     )
   end
