944cff9c115bdf0e3c603334130529efa83b84cd
59050336 <59050336@kmitl.ac.th>
2019-02-20 17:49:23 +0700

    update204

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,15 +17,8 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    if logged_in?(:site_admin) || @blog.published?
-      @blog = Blog.includes(:comments).friendly.find(params[:id])
-      @comment = Comment.new
-
-      @page_title = @blog.title
-      @seo_keywords = @blog.body
-    else
-      redirect_to blogs_path, notice: "You are not authorized to access this page"
-    end
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
@@ -91,6 +84,6 @@ class BlogsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def blog_params
-    params.require(:blog).permit(:title, :body)
+    params.require(:blog).permit(:title, :body, :topic_id)
   end
 end
